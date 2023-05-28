7b2fa84144c79c63507bd55bc6bcfaf59f5500f4
alissamsonyan <114940138+alissamsonyan@users.noreply.github.com>
2023-05-28 22:49:43 +0400

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,66 +1,79 @@
 #include "mainwindow.h"
 #include <QPainter>
+
 MainWindow::MainWindow(int n, int m)
 {
-    wid=new QWidget();
+    wid = new QWidget();
     this->n = n;
     this->m = m;
-
 }
 
 MainWindow::~MainWindow()
 {
 }
 
-
 void MainWindow::paintEvent(QPaintEvent *e) {
-
-  Q_UNUSED(e);
-
-  QPainter qp(this);
-  graphh(&qp);
+    Q_UNUSED(e);
+    QPainter qp(this);
+    graphh(&qp);
 }
 
 void MainWindow::graphh(QPainter *qp) const
 {
-    QPen pen(Qt::SolidLine);
-    qp->setPen(pen);
-    int i, l = 0, z, o = 11, v = 5, b; bool t = false;
+    int i, l = 0, z, o = 11, v = 5, b;
+    bool t = false;
 
-    if (n % 2 == 0 && m % 2 == 0) { t = true; }
     for (i = 0; i < n; i++)
     {
-        if (i == n - 2) o = o + m - 2;
-        if (i == n - 1) o = o + m;
-        if (i % 2 == 0) v++;
-        z = 0; b = 1;
+
+        z = 0;
+        b = 1;
+
         for (int j = 0; j < m; j++)
         {
             if (i == n - 1 && j == m - 2) o--;
             if (j % 2 == 0) b++;
-            pen.setColor(QColor(0, 0, 0));
-            qp->drawEllipse(100 + z, 100 + l, 20,20);
+
+            QPen pen(Qt::black);
+            qp->setPen(pen);
+            qp->drawEllipse(100 + z, 100 + l, 20, 20);
+
             if (j < (m - 1))
             {
-                if ((j == m - 2 || i == n - 1) && t == true) pen.setColor(QColor(o, 0, 0));
-                else
-                    pen.setColor(QColor(b, 0, 0));
+                QPen pen;
+                if ((j == m - 2 || i == n - 1) && t == true) {
+                     pen.setColor(Qt::green);
+                } else {
+                    pen.setColor(Qt::red);
+                   }
+                qp->setPen(pen);
                 qp->drawLine(120 + z, 100 + l, 130 + z, 100 + l);
             }
+
             if (i < (n - 1))
             {
+                QPen pen;
                 if ((j == m - 1 || i == n - 2) && t == true)
-                    if (i == n - 2 && j == m - 2) { o--; pen.setColor(QColor(o, 0, 0)); o += 2; }
+                {
+                    if (i == n - 2 && j == m - 2)
+                    {
+                       pen.setColor(Qt::red);
+                    }
                     else
-                        pen.setColor(QColor(o, 0, 0));
-                else pen.setColor(QColor(v, 0, 0));
+                    {
+                       pen.setColor(Qt::green);
+                    }
+                }
+                else
+                {
+                    pen.setColor(Qt::green);
+                }
+                qp->setPen(pen);
                 qp->drawLine(100 + z, 120 + l, 100 + z, 130 + l);
             }
             z += 50;
-            if (i >= n - 2) o--;
+
         }
-        l += 50; o--;
+        l += 50;
     }
 }
-
-
