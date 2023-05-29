a3da11767267dfc2557ea082f156c15c4a01aebd
alissamsonyan <114940138+alissamsonyan@users.noreply.github.com>
2023-05-29 16:39:23 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     int m, n;
-  bool ok;
- m = QInputDialog::getInt(nullptr, "Enter Value", "Enter the value of m:", 1, 1, 100, 1, &ok);
-  if (!ok) return 0;
- n = QInputDialog::getInt(nullptr, "Enter Value", "Enter the value of n:", 1, 1, 100, 1, &ok);
-  if (!ok) return 0;
-    if ((n % 2 == 0 && m % 2 != 0) || (n % 2 != 0 && m % 2 == 0) || (n == 1 && m == 1) || n <= 0 || m <= 0)
+   bool ok;
+  m = QInputDialog::getInt(nullptr, "Enter Value", "Enter the value of m:", 1, 1, 100, 1, &ok);
+   if (!ok) return 0;
+  n = QInputDialog::getInt(nullptr, "Enter Value", "Enter the value of n:", 1, 1, 100, 1, &ok);
+   if (!ok) return 0;
+    if ((n+m % 2 != 0) || (n+m==2) )
     {
         QMessageBox::critical(nullptr, "Error", "Can't cover");
         return 0;
