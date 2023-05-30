9a40fab7fdfeebb653d15d1a8068823c490002c7
alissamsonyan <114940138+alissamsonyan@users.noreply.github.com>
2023-05-30 18:29:37 +0400

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -27,26 +27,26 @@ void MainWindow::graphh(QPainter *qp) const
     for (int i = 0; i < n; i++)
     {
 
-        z = 50;
+        z = 100;
 
         for (int j = 0; j < m; j++)
         {
 
             QPen pen(Qt::black);
             qp->setPen(pen);
-            qp->drawEllipse(100 + z, 100 + l, 20, 20);
+            qp->drawEllipse( z, 100 + l, 20, 20);
 
             if (j < (m - 1))
             {
                 QPen pen;
                 if ((j == m - 2 || i == n - 1) && t == true) {
-                     pen.setColor(Qt::blue);
+                    pen.setColor(Qt::blue);
                 }
                 else {
                     pen.setColor(Qt::red);
-                   }
+                }
                 qp->setPen(pen);
-                qp->drawLine(130 + z, 110 + l, 140 + z, 110 + l);
+                qp->drawLine(30 + z, 110 + l, 40 + z, 110 + l);
             }
 
             if (i < (n - 1))
@@ -54,21 +54,14 @@ void MainWindow::graphh(QPainter *qp) const
                 QPen pen;
                 if ((j == m - 1 || i == n - 2) && t == true)
                 {
-                    if (i == n - 2 && j == m - 2)
-                    {
                        pen.setColor(Qt::blue);
-                    }
-                    else
-                    {
-                       pen.setColor(Qt::blue);
-                    }
                 }
                 else
                 {
-                    pen.setColor(Qt::green);
+                    pen.setColor(Qt::black);
                 }
                 qp->setPen(pen);
-                qp->drawLine(110 + z, 130 + l, 110 + z, 140 + l);
+                qp->drawLine(10 + z, 130 + l, 10 + z, 140 + l);
             }
             z += 50;
         }
