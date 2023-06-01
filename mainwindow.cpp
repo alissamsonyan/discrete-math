f724a5a0b05ccbbe3e2c90bac968c326427569ca
alissamsonyan <114940138+alissamsonyan@users.noreply.github.com>
2023-06-01 21:50:39 +0400

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,5 @@
 #include "mainwindow.h"
 
-
 MainWindow::MainWindow(int n, int m)
 {
     wid = new QWidget();
@@ -20,51 +19,128 @@ void MainWindow::paintEvent(QPaintEvent *e) {
 
 void MainWindow::graphh(QPainter *qp) const
 {
-    int l = 0, z;
+    int l = 80, z;
     bool t = false;
-    if(m%2==0&&n%2==0) t=true;
+    double k = 0.0;
+    double v=0.0;
+
+    if (m % 2 == 0 && n % 2 == 0)
+        t = true;
 
     for (int i = 0; i < n; i++)
     {
-
-        z = 100;
+        z = 80;
 
         for (int j = 0; j < m; j++)
         {
-
             QPen pen(Qt::black);
             qp->setPen(pen);
-            qp->drawEllipse( z, 100 + l, 20, 20);
+            qp->drawEllipse(z, l, 20, 20);
 
+            // Horizontal lines
             if (j < (m - 1))
             {
                 QPen pen;
+                // zuygg
                 if ((j == m - 2 || i == n - 1) && t == true) {
-                    pen.setColor(Qt::blue);
+                    if (static_cast<int>(k) % 2 == 0.0)
+                        pen.setColor(Qt::black);
+                    else
+                        pen.setColor(Qt::black);
                 }
+                // kent
                 else {
-                    pen.setColor(Qt::red);
+                    if ((static_cast<int>(k) % 2 == 0.0 ) )
+
+                            pen.setColor(Qt::red);
+                    else if ((static_cast<int>(k) % 2 != 0.0 ) )
+
+                        pen.setColor(Qt::cyan);
                 }
                 qp->setPen(pen);
-                qp->drawLine(30 + z, 110 + l, 40 + z, 110 + l);
+                qp->drawLine(30 + z, 10 + l, 40 + z, 10 + l);
             }
 
+            // Vertical lines
             if (i < (n - 1))
             {
                 QPen pen;
+                //zuyg
                 if ((j == m - 1 || i == n - 2) && t == true)
-                {
-                       pen.setColor(Qt::blue);
-                }
+                    if (static_cast<int>(v) % 2 == 0)   pen.setColor(Qt::black);
+                    else pen.setColor(Qt::black);
+                //kent
                 else
                 {
-                    pen.setColor(Qt::black);
+                    if (static_cast<int>(v) % 2 == 0)
+                        pen.setColor(Qt::green);
+                    else pen.setColor(Qt::magenta);
+
                 }
                 qp->setPen(pen);
-                qp->drawLine(10 + z, 130 + l, 10 + z, 140 + l);
+                qp->drawLine(10 + z, 30 + l, 10 + z, 40 + l);
             }
+
             z += 50;
+            k += 0.5;
+
         }
+
+        v+=0.5;
         l += 50;
+    }
+    if (t==false){
+        int norx=110, nory=140;
+
+        for(int  i=0;i<n/2;i++) {
+
+              QPen pen;
+
+            if(i%2==0){
+                norx=110;
+                for(int j=0;j<m/2;j++){
+                    if(j%2==0){
+                       pen.setColor(Qt::red);
+                       qp->setPen(pen);
+                    qp->drawLine(norx,nory,norx+10,nory);
+                    }
+
+                else{
+                pen.setColor(Qt::cyan);
+                qp->setPen(pen);
+                qp->drawLine(norx,nory,norx+10,nory);
+                }
+                norx+=100;
+            }
+
+            }
+
+            if(i%2!=0)
+
+            {
+
+                norx=110;
+                for(int j=0;j<m/2;j++)
+                {
+                if (j%2==0){
+                pen.setColor(Qt::cyan);
+                qp->setPen(pen);
+                qp->drawLine(norx,nory,norx+10,nory);}
+                 else {
+                pen.setColor(Qt::red);
+                qp->setPen(pen);
+                qp->drawLine(norx,nory,norx+10,nory);
+                    }
+
+                norx+=100;
+                }
+
+            }
+
+         nory+=100;
+    }
+
+
+
     }
 }
